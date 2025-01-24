3813a5518f75302104ead124caa1ec65fbdc6632
maxime <maxime@alnitaka.com>
2025-01-24 17:57:50 +0100

    Add custom pattern

---
+++ patterns/custom-pattern-1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Colonne avec Image
+ * Slug: softech/column-with-image
+ * Categories: column
+ *
+ * @package Softech
+ * @subpackage Softech
+ * @since Softech 1.0
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"7.5rem","bottom":"7.5rem"}}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-base-background-color has-background" style="padding-top:7.5rem;padding-bottom:7.5rem"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"center","width":"588px","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<div class="wp-block-column is-vertically-aligned-center has-contrast-color has-text-color has-link-color" style="flex-basis:588px"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}},"textColor":"contrast"} -->
+<p class="has-contrast-color has-text-color has-link-color" style="font-size:18px;font-style:normal;font-weight:500">Depuis 1907</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"56px"}}} -->
+<h3 class="wp-block-heading" style="font-size:56px">Notre monde tourne autour de la rotation</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Les roulements ont contribué à la croissance des industries, ont révolutionné les transports et ont amélioré la qualité de vie globale des humains. Et SKF a participé à une grande partie du processus.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">Au fil du temps, nous avons repoussé les limites du possible. Et aujourd’hui, nous continuons à développer des technologies de roulement qui soutiennent le progrès et contribuent à améliorer la vie quotidienne, aujourd’hui et demain.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":59,"width":"704px","height":"939px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img style="object-fit:cover;width:704px;height:939px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
